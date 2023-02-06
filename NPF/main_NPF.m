b00abf9c26761f47dbdd8f84037ca40b6555b84d
Sara Pérez Vieites <44875004+sarapv@users.noreply.github.com>
2023-02-06 13:02:08 +0000

    Update main_NPF.m

--- NPF/main_NPF.m
+++ NPF/main_NPF.m
@@ -4,9 +4,9 @@ nosc = 10;      % nosc = d_x = no. of oscillators or dimension of slow variables
 N = 500;        % no. particles of first layer
 M = 500;        % no. particles of second layer
 K = 2;          % we observe one every K slow oscillators, e.g., [x_1, x_3, ..., x_9]
-t_final = 0.6;   % duration of the simulation in natural time units
+t_final = 10;   % duration of the simulation in natural time units
 
-total_iterations = 1;
+total_iterations = 1; % 1:10 
 
 %% Run
 
