5eff8531b9a66d7cb30266b90c66a092e36ea826
Sara Pérez Vieites <44875004+sarapv@users.noreply.github.com>
2023-02-06 13:00:11 +0000

    Add main file

---
+++ two-stage filter/main_2stagefilter.m
@@ -0,0 +1,18 @@
+%% Initializations
+
+nosc = 10;      % nosc = d_x = no. of oscillators or dimension of slow variables (x)
+N = 500;        % no. particles of first layer
+M = 500;        % no. particles of second layer
+K = 2;          % we observe one every K slow oscillators, e.g., [x_1, x_3, ..., x_9]
+t_final = 10;   % duration of the simulation in natural time units
+
+total_iterations = 1;
+
+%% Run
+
+for iter = total_iterations
+
+    [ Output_2SF ] = TwoStageFiltering(nosc,N,M,K,t_final,iter);
+    
+
+end
