6c7169b00f6d88351b4b36f9d1fd35ee4a88713c
Sara Pérez Vieites <44875004+sarapv@users.noreply.github.com>
2023-02-08 13:21:54 +0000

    Update NHF_SMCEnKF_Lorenz96.m

--- NHF/SMC-EnKF/NHF_SMCEnKF_Lorenz96.m
+++ NHF/SMC-EnKF/NHF_SMCEnKF_Lorenz96.m
@@ -68,7 +68,7 @@ ttotal = etime(clock,t0)/60;
 fprintf(1, 'time to finish = %7.3f min\n',ttotal);
 
 Output_SMCEnKF = struct('FAest',FAest,'Xest',Xest,'FAparticles',FAparticles,'MSEx',MSEx, 'MSE_ansatz', MSE_ansatz,'xref',xref);  
-fprintf(1,'SMC+EnKF (F unk., ansatz): MSE = %7.3f \n', mean(MSEx(1+Tobs:Tobs:NT)));
+fprintf(1,'SMC+EnKF (F unk., ansatz): MSEx = %7.3f \n', mean(MSEx(1+Tobs:Tobs:NT)));
 fprintf(1,'\n ------------------------------------------------------------- \n \n');
 
 % Save data
