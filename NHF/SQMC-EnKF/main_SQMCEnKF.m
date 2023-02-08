4c653320b744e7142d062ce2ec3e18dfecbf6c4d
Sara Pérez Vieites <44875004+sarapv@users.noreply.github.com>
2023-02-08 13:20:43 +0000

    Update main_SQMCEnKF.m

--- NHF/SQMC-EnKF/main_SQMCEnKF.m
+++ NHF/SQMC-EnKF/main_SQMCEnKF.m
@@ -4,7 +4,7 @@ nosc = 10;      % nosc = d_x = no. of oscillators or dimension of slow variables
 M = 100;        % no. particles of first layer
 gap = 0.05;     % no. particles of second layer
 %K = 2;          % we observe one every K slow oscillators, e.g., [x_1, x_3, ..., x_9]
-t_final = 4;   % duration of the simulation in natural time units
+t_final = 40;   % duration of the simulation in natural time units
 
 total_iterations = 1; %1:10
 
