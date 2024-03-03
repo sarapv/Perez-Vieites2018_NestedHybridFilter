da6822c6f19ac059c98d875d750853930ef1ba42
sarapv <saraperezvieites198@gmail.com>
2024-03-03 12:51:05 +0200

    Lorenz 96 in python (adding partial)

--- NPF/python/lorenz96model_2scale.py
+++ NPF/python/lorenz96model_2scale.py
@@ -1,10 +1,10 @@
 import time
 
 import numpy as np
+from functools import partial
 
 
 def fx_fz_lorenz96(x_old, z_old, dx, dz, fps, param):
-
     F = param[0]
     H = param[1]
     B = param[2]
@@ -43,52 +43,52 @@ def fx_fz_lorenz96(x_old, z_old, dx, dz, fps, param):
 
 def runge_kutta4_lorenz96_1timestep(x_old, z_old, dx, dz, fps, h, param, noise_var):
     # Placeholder for the Euler method implementation
-    #print("4th order Runge-Kutta method called")
+    # print("4th order Runge-Kutta method called")
 
     s2x = noise_var[0]
     s2z = noise_var[2]
 
     fx1, fz1 = fx_fz_lorenz96(x_old, z_old, dx, dz, fps, param)
 
-    x1_rk = x_old + 0.5 * (h * fx1 + np.sqrt(h*s2x) * np.random.randn(dx))
-    z1_rk = z_old + 0.5 * (h * fz1 + np.sqrt(h*s2z) * np.random.randn(dz))
+    x1_rk = x_old + 0.5 * (h * fx1 + np.sqrt(h * s2x) * np.random.randn(dx))
+    z1_rk = z_old + 0.5 * (h * fz1 + np.sqrt(h * s2z) * np.random.randn(dz))
     fx2, fz2 = fx_fz_lorenz96(x1_rk, z1_rk, dx, dz, fps, param)
 
-    x2_rk = x_old + 0.5 * (h * fx2 + np.sqrt(h*s2x) * np.random.randn(dx))
-    z2_rk = z_old + 0.5 * (h * fz2 + np.sqrt(h*s2z) * np.random.randn(dz))
-    fx3, fz3 = fx_fz_lorenz96(x2_rk, z2_rk, dx, dz,fps, param)
+    x2_rk = x_old + 0.5 * (h * fx2 + np.sqrt(h * s2x) * np.random.randn(dx))
+    z2_rk = z_old + 0.5 * (h * fz2 + np.sqrt(h * s2z) * np.random.randn(dz))
+    fx3, fz3 = fx_fz_lorenz96(x2_rk, z2_rk, dx, dz, fps, param)
 
-    x3_rk = x_old + 1 * (h * fx3 + np.sqrt(h*s2x) * np.random.randn(dx))
-    z3_rk = z_old + 1 * (h * fz3 + np.sqrt(h*s2z) * np.random.randn(dz))
+    x3_rk = x_old + 1 * (h * fx3 + np.sqrt(h * s2x) * np.random.randn(dx))
+    z3_rk = z_old + 1 * (h * fz3 + np.sqrt(h * s2z) * np.random.randn(dz))
     fx4, fz4 = fx_fz_lorenz96(x3_rk, z3_rk, dx, dz, fps, param)
 
-    x_new = x_old + (1 / 6) * (h * (fx1 + fx2 + fx3 + fx4) + np.sqrt(h*s2x) * np.random.randn(dx))
-    z_new = z_old + (1 / 6) * (h * (fz1 + fz2 + fz3 + fz4) + np.sqrt(h*s2z) * np.random.randn(dz))
+    x_new = x_old + (1 / 6) * (h * (fx1 + fx2 + fx3 + fx4) + np.sqrt(h * s2x) * np.random.randn(dx))
+    z_new = z_old + (1 / 6) * (h * (fz1 + fz2 + fz3 + fz4) + np.sqrt(h * s2z) * np.random.randn(dz))
 
     return x_new, z_new
 
 
 def euler_lorenz96_1timestep(x_old, z_old, dx, dz, fps, h, param, noise_var):
     # Placeholder for the Runge-Kutta 4 method implementation
-    #print("Euler method called")
+    # print("Euler method called")
     s2x = noise_var[0]
     s2z = noise_var[2]
 
     fx, fz = fx_fz_lorenz96(x_old, z_old, dx, dz, fps, param)
 
-    x_new = x_old + h * fx + np.sqrt(h*s2x) * np.random.randn(dx)
-    z_new = z_old + h * fz + np.sqrt(h*s2z) * np.random.randn(dz)
+    x_new = x_old + h * fx + np.sqrt(h * s2x) * np.random.randn(dx)
+    z_new = z_old + h * fz + np.sqrt(h * s2z) * np.random.randn(dz)
 
     return x_new, z_new
 
 
 def lorenz96_2scale(dx, fps, h, t_final, integration_method):
     """
-    Simulates a Lorenz96 model :true_param integration_method: "euler" or "runge-kutta4" are available to integrate the
-    SDEs of Lorenz96 :true_param dx: dimension of the state x, e.g., dx = 20 :true_param fps: number of fast variables per each
-    slow variables x (i.e., dimension of z = fps * dx), e.g., fps = 10 :true_param h: step-size for the integration of the
-    SDEs (in natural units), e.g., h = 2e-4 :true_param t_final: length of the simulation in natural time units (i.e.,
-    t_final/h discrete time steps), e.g., t_final = 10
+    Simulates a Lorenz96 model :true_param integration_method: "euler" or "runge-kutta4" are available to integrate
+    the SDEs of Lorenz96 :true_param dx: dimension of the state x, e.g., dx = 20 :true_param fps: number of fast
+    variables per each slow variables x (i.e., dimension of z = fps * dx), e.g., fps = 10 :true_param h: step-size
+    for the integration of the SDEs (in natural units), e.g., h = 2e-4 :true_param t_final: length of the simulation
+    in natural time units (i.e., t_final/h discrete time steps), e.g., t_final = 10
 
     :return: x: slow state variables from t = 0, ... , T
     :return: y: (partial) observations of the slow state variables from t = 0, ... , T
@@ -125,20 +125,22 @@ def lorenz96_2scale(dx, fps, h, t_final, integration_method):
     x[:, 0] = np.random.rand(dx)
     z[:, 0] = (1 / (C * B)) * np.random.rand(dz) - (1 / (2 * C * B))
 
+    # Integration method
+    if integration_method == "euler":
+        lorenz96_1timestep = partial(euler_lorenz96_1timestep, dx=dx, dz=dz, fps=fps, h=h, param=param,
+                                     noise_var=noise_var)
+    elif integration_method == "runge-kutta4":
+        lorenz96_1timestep = partial(runge_kutta4_lorenz96_1timestep, dx=dx, dz=dz, fps=fps, h=h, param=param,
+                                     noise_var=noise_var)
+    else:
+        raise ValueError("Invalid method. Choose 'euler' or 'runge-kutta4'.")
+
     # Capture start time
     t0 = time.time()
     ok = 0
     while not ok:
         for t in np.arange(1, T):
-            if integration_method == "euler":
-                x[:, t], z[:, t] = euler_lorenz96_1timestep(x[:, t - 1], z[:, t - 1], dx, dz, fps, h, param, noise_var)
-
-            elif integration_method == "runge-kutta4":
-                x[:, t], z[:, t] = runge_kutta4_lorenz96_1timestep(x[:, t - 1], z[:, t - 1], dx, dz, fps, h, param,
-                                                                   noise_var)
-
-            else:
-                raise ValueError("Invalid method. Choose 'euler' or 'runge-kutta4'.")
+            x[:, t], z[:, t] = lorenz96_1timestep(x_old=x[:, t - 1], z_old=z[:, t - 1])
 
         ok = not np.any(np.isnan(x[0, :]) | np.isinf(x[0, :])) | np.any(np.isnan(z[0, :]) | np.isinf(z[0, :]))
 
