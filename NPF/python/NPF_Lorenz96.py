cd733210cedd1b34c99a4fc3b4d2c1604c4314e2
sarapv <saraperezvieites198@gmail.com>
2024-03-11 17:37:12 +0200

    NPF : jittering function (normal truncated)

--- NPF/python/NPF_Lorenz96.py
+++ NPF/python/NPF_Lorenz96.py
@@ -2,17 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lorenz96model_2scale import lorenz96_2scale
 
-
-
 # Ground truth of the model : stochastic Lorenz 96 model
 # dimension of the model
-dx = 20         # dimension of slow variables (x)
-fps = 10        # no. of fast variables (z) per slow variables (x)
+dx = 20  # dimension of slow variables (x)
+fps = 10  # no. of fast variables (z) per slow variables (x)
 
 # time variables
-h = 2e-4        # integration period in natural units
-t_final = 10    # duration of the simulation in natural time units
-t_obs = 200     # states are observed every t_obs time steps
+h = 2e-4  # integration period in natural units
+t_final = 10  # duration of the simulation in natural time units
+t_obs = 200  # states are observed every t_obs time steps
 
 # generating ground truth, observations, as well as obtaining true parameters
 x, z, y, u, true_param, noise_var, T = lorenz96_2scale(dx, fps, h, t_final, integration_method="runge-kutta4")
@@ -26,8 +24,8 @@ idx_plotted = [1, 2]
 auxT = T
 for i, idx in enumerate(idx_plotted):
     plt.subplot(len(idx_plotted), 1, i + 1)
-    plt.plot(np.arange(0, auxT)*h, x[idx, 0:auxT], 'k', label='state' if i == 0 else "")
-    plt.plot(np.arange(0, auxT, t_obs)*h, y[idx, 0:auxT:t_obs], '*', label='observations' if i == 0 else "")
+    plt.plot(np.arange(0, auxT) * h, x[idx, 0:auxT], 'k', label='state' if i == 0 else "")
+    plt.plot(np.arange(0, auxT, t_obs) * h, y[idx, 0:auxT:t_obs], '*', label='observations' if i == 0 else "")
     if i == 0:  # Add legend only to the first subplot to avoid repetition
         plt.legend()
 plt.suptitle('Slow state and observations')
@@ -36,12 +34,25 @@ plt.figure(2)
 idx_plotted_z = [1, 2]
 for i, idx in enumerate(idx_plotted_z):
     plt.subplot(len(idx_plotted_z), 1, i + 1)
-    plt.plot(np.arange(0, auxT)*h, z[idx, 0:auxT], 'k', label='state' if i == 0 else "")
-    plt.plot(np.arange(0, auxT, t_obs)*h, u[idx, 0:auxT:t_obs], '*', label='observations' if i == 0 else "")
+    plt.plot(np.arange(0, auxT) * h, z[idx, 0:auxT], 'k', label='state' if i == 0 else "")
+    plt.plot(np.arange(0, auxT, t_obs) * h, u[idx, 0:auxT:t_obs], '*', label='observations' if i == 0 else "")
     if i == 0:  # Add legend only to the first subplot to avoid repetition
         plt.legend()
 plt.suptitle('Fast state and observations')
 plt.show()
+
+
+
+
+prueba = np.random.rand(4, 20)
+bounds = np.array([
+    [0, 1],
+    [0.1, 1],
+    [0, 0.9],
+    [0.1, 0.9]
+])
+std_var_jit = [0.2, 0.3, 0.4, 0.5]
+
 
 
 # TODO: initialize algorithm parameters, i.e., number of particles per layer, jittering variance, range of parameters
--- NPF/python/nested_particle_filter.py
+++ NPF/python/nested_particle_filter.py
@@ -1,10 +1,22 @@
-import time
 
 import numpy as np
 from functools import partial
 
+from numpy import ndarray, dtype
 
-def nested_particle_filter(x,y,N,M,K,h,T,range,jittering_var,param):
+def truncnorm(mean, std_dev, bounds):
+    new_mean = np.zeros_like(mean)
+    for i in range(mean.shape[0]):
+        idx = np.arange(mean.shape[1])
+        while True:
+            new_mean[i, idx] = np.random.normal(mean[i, idx], std_dev[i])
+            condition = (new_mean[i, :] < bounds[i, 0]) | (new_mean[i, :] > bounds[i, 1])
+            idx = np.where(condition)[0]
+            if len(idx) == 0:
+                break
+    return new_mean
+
+def nested_particle_filter(x ,y ,N ,M ,K ,h ,T ,range ,jittering_var ,param):
 
     dx = x.shape[0]
     dy = y.shape[0]
@@ -12,30 +24,32 @@ def nested_particle_filter(x,y,N,M,K,h,T,range,jittering_var,param):
 
     # Estimates
     x_est = np.zeros_like(x)
-    param_est = np.zeros((dparam,T))
+    param_est = np.zeros((dparam ,T))
 
-    weights_param = np.ones(N)*(1/N)
+    weights_param = np.ones(N ) *( 1 /N)
 
-    Param_particles = np.zeros((dparam,N))
+    Param_particles = np.zeros((dparam ,N))
     for i in range(dparam):
-        Param_particles[i,:] = np.random.uniform(range[i,0], range[i,2], N)
+        Param_particles[i ,:] = np.random.uniform(range[i ,0], range[i ,1], N)
 
     X_all_particles = np.zeros((N, dx, M))
     X_estimates_second_layer = np.zeros((dx, N))
 
     for t in np.arange(1, T):
-        # TODO: Jittering of Param_particles
+        # jittering of Param_particles
+        Param_particles = truncnorm(Param_particles, np.sqrt(jittering_var), range)
 
         log_weights_param = np.zeros(N)
         for m in range(M):
             # TODO: call particle filter for the state give parameters
+            print(m)
 
         unnormalized_weights_param = np.exp(log_weights_param - max(log_weights_param))
         weights_param = unnormalized_weights_param / np.sum(unnormalized_weights_param)
 
         # Estimates
-        x_est[:,t] = np.cross(X_estimates_second_layer,weights_param.T)
-        param_est[:,t] = np.cross(Param_particles,weights_param.T)
+        x_est[: ,t] = np.cross(X_estimates_second_layer ,weights_param.T)
+        param_est[: ,t] = np.cross(Param_particles ,weights_param.T)
 
         # TODO: resampling
 
@@ -47,8 +61,7 @@ def nested_particle_filter(x,y,N,M,K,h,T,range,jittering_var,param):
     return result
 
 
-def particle_filter_for_state_estimation(state,N,M,K,h):
+def particle_filter_for_state_estimation(state ,N ,M ,K ,h):
     result = 1
 
     return result
-
